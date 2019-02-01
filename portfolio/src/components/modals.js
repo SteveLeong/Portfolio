6f7c482133d6b4a1e042a8c928460de71089859b
Steven Leong <steveleong8@gmail.com>
2019-02-01 10:53:26 -0700

    Default Modal Added

---
+++ portfolio/src/components/modals.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Modal, Button } from "antd";
+
+class Modal extends Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okButtonProps={{ disabled: true }}
+          cancelButtonProps={{ disabled: true }}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Modal;
--- portfolio/src/components/projectCard.js
+++ portfolio/src/components/projectCard.js
@@ -1,35 +1,71 @@
 import React, { Component } from "react";
 
-import { Card } from "antd";
+import { Card, Modal } from "antd";
 import "antd/dist/antd.css";
 const { Meta } = Card;
 
 class ProjectCard extends Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  //   handleOk = e => {
+  //     console.log(e);
+  //     this.setState({
+  //       visible: false
+  //     });
+  //   };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
   render() {
     return (
-      <Card
-        bordered={false}
-        style={{
-          background: "transparent",
-          padding: "5%",
-          marginBottom: "10%",
-          width: "90%"
-        }}
-        hoverable
-      >
+      <div>
         <Card
-          hoverable
           bordered={false}
-          cover={
-            <img
-              alt="example"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            />
-          }
+          style={{
+            background: "transparent",
+            padding: "5%",
+            marginBottom: "10%",
+            width: "90%"
+          }}
+          hoverable
+          onClick={this.showModal}
         >
-          <Meta title="Europe Street beat" description="www.instagram.com" />
+          <Card
+            hoverable
+            bordered={false}
+            cover={
+              <img
+                alt="example"
+                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              />
+            }
+          >
+            <Meta title="Europe Street beat" description="www.instagram.com" />
+          </Card>
         </Card>
-      </Card>
+        <Modal
+          title="Basic Modal"
+          centered
+          visible={this.state.visible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
     );
   }
 }
