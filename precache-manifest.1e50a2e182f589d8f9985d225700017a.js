4479f26ea4515befd03822bf386307f550ab60f3
Steven Leong <steveleong8@gmail.com>
2019-02-17 20:09:36 -0700

    Updates

--- precache-manifest.1e50a2e182f589d8f9985d225700017a.js
+++ precache-manifest.1e50a2e182f589d8f9985d225700017a.js
@@ -120,6 +120,10 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/dark.acd303ca.jpg"
   },
   {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/portfolio/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
     "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
     "url": "/portfolio/static/media/fontawesome-webfont.a35720c2.woff"
   },
@@ -128,16 +132,12 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/fontawesome-webfont.32400f4e.eot"
   },
   {
-    "revision": "db812d8a70a4e88e888744c1c9a27e89",
-    "url": "/portfolio/static/media/fontawesome-webfont.db812d8a.woff2"
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/portfolio/static/media/fontawesome-webfont.a3de2170.ttf"
   },
   {
     "revision": "f775f9cca88e21d45bebe185b27c0e5b",
     "url": "/portfolio/static/media/fontawesome-webfont.f775f9cc.svg"
-  },
-  {
-    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
-    "url": "/portfolio/static/media/fontawesome-webfont.a3de2170.ttf"
   },
   {
     "revision": "e3f3ba35418d63ee1a45",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bc21abf563caa90dde12ff0c25e3eab8.js"
+  "/portfolio/precache-manifest.1e50a2e182f589d8f9985d225700017a.js"
 );
 
 workbox.clientsClaim();
