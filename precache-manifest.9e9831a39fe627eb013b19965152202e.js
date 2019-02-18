fb17c4e9a75e381098de3a450f5f4eacaf89eac2
Steven Leong <steveleong8@gmail.com>
2019-02-17 20:12:18 -0700

    Updates

---
+++ precache-manifest.9e9831a39fe627eb013b19965152202e.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3e63080545cf4c991b8f65d27ced463",
+    "url": "/Portfolio/static/media/Library2.e3e63080.jpg"
+  },
+  {
+    "revision": "ad77ebd38add738ce38c",
+    "url": "/Portfolio/static/css/main.b0e4b591.chunk.css"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "a99cd45428122c1f02d5921fc993f1fe",
+    "url": "/Portfolio/static/media/profile.a99cd454.jpg"
+  },
+  {
+    "revision": "152131b489e3573a8518",
+    "url": "/Portfolio/static/js/2.152131b4.chunk.js"
+  },
+  {
+    "revision": "32b324d51533841b53b1d67023d654ea",
+    "url": "/Portfolio/static/media/Critix1.32b324d5.jpg"
+  },
+  {
+    "revision": "d5addb57916873dad283e47094230ff0",
+    "url": "/Portfolio/static/media/Critix2.d5addb57.jpg"
+  },
+  {
+    "revision": "cbe9831465039548c12d070b468d460c",
+    "url": "/Portfolio/static/media/Critix3.cbe98314.jpg"
+  },
+  {
+    "revision": "5e87d14582beb67ac789a0c7465acb19",
+    "url": "/Portfolio/static/media/Critix4.5e87d145.jpg"
+  },
+  {
+    "revision": "47a91d9f5059172a8e539d615b06263a",
+    "url": "/Portfolio/static/media/Critix5.47a91d9f.jpg"
+  },
+  {
+    "revision": "d99b6b19da1f7effbdc587b732e3fde2",
+    "url": "/Portfolio/static/media/ImportCar1.d99b6b19.jpg"
+  },
+  {
+    "revision": "266a2a255fa891199c1befac37a77ac1",
+    "url": "/Portfolio/static/media/ImportCar2.266a2a25.jpg"
+  },
+  {
+    "revision": "64d314a31f17307230d3db481d4e5298",
+    "url": "/Portfolio/static/media/ImportCar3.64d314a3.jpg"
+  },
+  {
+    "revision": "a99cb71352b110acc2415beed2e2e321",
+    "url": "/Portfolio/static/media/LeafBank1.a99cb713.jpg"
+  },
+  {
+    "revision": "535d94949fe3de3c3b8d2808de710bdb",
+    "url": "/Portfolio/static/media/LeafBank2.535d9494.jpg"
+  },
+  {
+    "revision": "04db353203053a6c286856ba1222d39e",
+    "url": "/Portfolio/static/media/LeafBank3.04db3532.jpg"
+  },
+  {
+    "revision": "38659d1d73fea0f10b20575813698552",
+    "url": "/Portfolio/static/media/LeafBank4.38659d1d.jpg"
+  },
+  {
+    "revision": "925058dc53125857326886c634a7f849",
+    "url": "/Portfolio/static/media/Library1.925058dc.jpg"
+  },
+  {
+    "revision": "ad77ebd38add738ce38c",
+    "url": "/Portfolio/static/js/main.ad77ebd3.chunk.js"
+  },
+  {
+    "revision": "933017049fdbbeb34aee36240af67474",
+    "url": "/Portfolio/static/media/Library3.93301704.jpg"
+  },
+  {
+    "revision": "84fec23d8cfd9199b475dcf214e0c741",
+    "url": "/Portfolio/static/media/Library4.84fec23d.jpg"
+  },
+  {
+    "revision": "ad9c8b8dfd9b3e9f9afb0f732dfb7326",
+    "url": "/Portfolio/static/media/Matrix1.ad9c8b8d.jpg"
+  },
+  {
+    "revision": "d99b6b19da1f7effbdc587b732e3fde2",
+    "url": "/Portfolio/static/media/ImportCar1.d99b6b19.d99b6b19.jpg"
+  },
+  {
+    "revision": "a99cb71352b110acc2415beed2e2e321",
+    "url": "/Portfolio/static/media/LeafBank1.a99cb713.a99cb713.jpg"
+  },
+  {
+    "revision": "925058dc53125857326886c634a7f849",
+    "url": "/Portfolio/static/media/Library1.925058dc.925058dc.jpg"
+  },
+  {
+    "revision": "ad9c8b8dfd9b3e9f9afb0f732dfb7326",
+    "url": "/Portfolio/static/media/Matrix1.ad9c8b8d.ad9c8b8d.jpg"
+  },
+  {
+    "revision": "bb9cdd239b8105b3af55f53b1ad1d312",
+    "url": "/Portfolio/static/media/mid-image.bb9cdd23.bb9cdd23.jpg"
+  },
+  {
+    "revision": "a99cd45428122c1f02d5921fc993f1fe",
+    "url": "/Portfolio/static/media/profile.a99cd454.a99cd454.jpg"
+  },
+  {
+    "revision": "bb9cdd239b8105b3af55f53b1ad1d312",
+    "url": "/Portfolio/static/media/mid-image.bb9cdd23.jpg"
+  },
+  {
+    "revision": "acd303ca04f9132f1f5d6e554e0f6a01",
+    "url": "/Portfolio/static/media/dark.acd303ca.jpg"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/Portfolio/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/Portfolio/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/Portfolio/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/Portfolio/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/Portfolio/static/media/fontawesome-webfont.f775f9cc.svg"
+  },
+  {
+    "revision": "152131b489e3573a8518",
+    "url": "/Portfolio/static/css/2.4be36f89.chunk.css"
+  },
+  {
+    "revision": "f2cd7e3555d54b61a020597b404f4e29",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1e50a2e182f589d8f9985d225700017a.js"
+  "/Portfolio/precache-manifest.9e9831a39fe627eb013b19965152202e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
